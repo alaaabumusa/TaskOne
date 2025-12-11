456a1f155b19123ce60ddba916d7ebdc75e64b0c
Alaa Abu Mussa <166786217+alaaabumusa@users.noreply.github.com>
2025-12-11 22:09:54 +0200

    Add files via upload

---
+++ TaskOne/TaskOne/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Alaa Abu Mussa";
+            int age = 22;
+
+            Console.WriteLine("My Name is: " + name);
+            Console.WriteLine("My Age is: " + age);
+
+        }
+    }
+}
